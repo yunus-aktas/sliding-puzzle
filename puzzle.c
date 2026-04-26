1c30a7f1f1cb9b0efdd6bc01c3bfd78b2fb2c831
Yunus Aktaş <aktasyunus597@gmail.com>
2026-04-26 18:42:27 +0300

    tahtayiBaslat ve cozulebilirMi fonksiyonlari eklendi

--- puzzle.c
+++ puzzle.c
@@ -23,3 +23,56 @@ void tahtayiCiz(){
         printf("+-------+-------+-------+\n");
     }
 }
+
+
+
+
+int cozulebilirMi() {
+    int dizi[8],k = 0;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (tahta[i][j] != 0) {
+                dizi[k++] = tahta[i][j];
+            }
+        }
+    }
+    int inversiyon = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = i + 1; j < 8; j++) {
+            if (dizi[i] > dizi[j]) {
+                inversiyon++;
+            }
+        }
+    }
+    return inversiyon % 2 == 0;
+}
+
+
+
+
+
+void tahtayiBaslat(){
+    int sayilar[9]={1,2,3,4,5,6,7,8,0};
+
+    srand(time(NULL));
+    do {
+        for(int i=8;i>0;i--){
+        int j,temp;
+        j = rand() % (i + 1);
+        temp = sayilar[i];
+        sayilar[i] = sayilar[j];
+        sayilar[j] = temp;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            tahta[i][j] = sayilar[i * 3 + j];
+            if (tahta[i][j] == 0) {
+                bosX = i;
+                bosY = j;
+            }
+        }
+    }
+    } while (!cozulebilirMi());
+}
